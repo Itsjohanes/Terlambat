a6f7bde725169e0b19232592ecfdd89b5a7243e2
Johannes Alexander Putra <71767003+Itsjohanes@users.noreply.github.com>
2023-03-31 16:47:16 +0700

    tambah sedikit

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -29,6 +29,8 @@ class Admin extends CI_Controller
   public function index()
   {
     $data['title'] = "Dashboard";
+    //mendapatkan jumlah user dari database
+    $data['jumlahAdmin'] = $this->db->get_where('tb_akun', ['aktif' => 1])->num_rows();
     if ($this->session->userdata('email') == '') {
       redirect('Auth');
     } else {
@@ -36,6 +38,41 @@ class Admin extends CI_Controller
       $this->load->view('admin/sidebar');
       $this->load->view('admin/dashboard');
       $this->load->view('admin/footer');
+    }
+  }
+
+  public function siswa()
+  {
+    $data['title'] = "Data Siswa";
+    //mendapatkan data siswa dari database
+    $data['siswa'] = $this->db->get('tb_siswa')->result_array();
+    if ($this->session->userdata('email') == '') {
+      redirect('Auth');
+    } else {
+      $this->load->view('admin/header', $data);
+      $this->load->view('admin/sidebar');
+      $this->load->view('admin/siswa');
+      $this->load->view('admin/footer');
+    }
+  }
+  public function tambahSiswa()
+  {
+    $data['title'] = "Tambah Siswa";
+    if ($this->session->userdata('email') == '') {
+      redirect('Auth');
+    } else {
+      //mendapatkan data dari form
+      $nama = $this->input->post('nama');
+      $kelas = $this->input->post('kelas');
+      $data = [
+        'nama_siswa' => $nama,
+        'kelas_siswa' => $kelas
+      ];
+      //masukan ke tb_siswa
+      $this->db->insert('tb_siswa', $data);
+      //set flashdata category success
+      $this->session->set_flashdata('category_success', 'Data berhasil ditambahkan');
+      redirect('Admin/siswa');
     }
   }
 }
---
+++ application/views/admin/siswa.php
@@ -0,0 +1,117 @@
+<!-- Begin Page Content -->
+
+
+
+<div id="layoutSidenav_content">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+    <?php if ($this->session->flashdata('category_success')) { ?>
+        <div class="alert alert-success"> <?= $this->session->flashdata('category_success') ?> </div>
+    <?php } ?>
+    <?php if ($this->session->flashdata('category_error')) { ?>
+        <div class="alert alert-danger"> <?= $this->session->flashdata('category_error') ?> </div>
+    <?php } ?>
+    <div class="row no-gutters">
+
+        <br>
+        <div class="container-fluid">
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Tambah Data Siswa</h6>
+                </div>
+                <?php echo form_open_multipart('Admin/tambahSiswa'); ?>
+                <div class="row">
+                    <div class="col">
+                        <input type="text" required class="form-control" placeholder="Nama" id="nama" name="nama">
+                    </div>
+                    <div class="col">
+                        <input type="text" required class="form-control" placeholder="Kelas" id="kelas" name="kelas">
+                    </div>
+
+                    <div class="col">
+                        <Button class="btn btn-success">Submit</Button>
+                    </div>
+                </div>
+                </form>
+            </div>
+        </div>
+
+
+
+
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+
+            <!-- DataTales Example -->
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Data Siswa</h6>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table id="datatablesSimple">
+                            <thead>
+                                <tr>
+                                    <th scope="col">No.</th>
+                                    <th scope="col">Nama</th>
+                                    <th scope="col">Kelas</th>
+                                    <th scope="col">Aksi</th>
+
+                                </tr>
+                            </thead>
+                            <tfoot>
+                                <tr>
+                                    <th scope="col">No.</th>
+                                    <th scope="col">Nama</th>
+                                    <th scope="col">Kelas</th>
+                                    <th scope="col">Aksi</th>
+
+                                </tr>
+                            </tfoot>
+                            <tbody>
+                                <?php $i = 1; ?>
+                                <?php foreach ($siswa as $j) : ?>
+                                    <tr>
+                                        <th scope="row"><?= $i; ?></th>
+                                        <td><?= $j['nama_siswa']; ?></td>
+                                        <td><?= $j['kelas_siswa']; ?></td>
+                                        <td>
+                                            <a href="<?= base_url(); ?>Admin/hapusSiswa/<?= $j['id_siswa']; ?>" class="btn btn-danger" onclick="return confirm('Yakin?');"><i class="fas fa-trash-alt"></i></a>
+                                            <a href="<?= base_url(); ?>Admin/editSiswa/<?= $j['id_siswa']; ?>" class="btn btn-warning"><i class="fas fa-edit"></i></a>
+                                        </td>
+
+                                    </tr>
+                                    <?php $i++; ?>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- End of Main Content -->
+    <!-- Page level plugins -->
