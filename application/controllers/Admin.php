6aa96db41270504848e2ed6d99adf7c5874d536d
Johannes Alexander Putra <71767003+Itsjohanes@users.noreply.github.com>
2023-03-31 18:42:44 +0700

    blm

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -24,6 +24,7 @@ class Admin extends CI_Controller
   public function __construct()
   {
     parent::__construct();
+    $this->load->library('cetak_pdf');
   }
 
   public function index()
@@ -200,7 +201,6 @@ class Admin extends CI_Controller
       redirect('Auth');
     } else {
       $date = $this->input->post('date');
-      
     }
   }
 }
